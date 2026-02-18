77ec6d4ee360fba26c13b6e4102d87d0eb82e425
honuKdestine <honudestine933@gmail.com>
2026-02-18 22:51:34 +0000

    Add button behavior

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Namer App',
         theme: ThemeData(
+          useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
         home: MyHomePage(),
@@ -27,6 +28,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -43,7 +49,7 @@ class MyHomePage extends StatelessWidget {
 
             ElevatedButton(
               onPressed: () {
-                print("button pressed!");
+                appState.getNext();
               },
               child: Text("Next"),
             ),
