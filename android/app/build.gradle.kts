edab8eb48bbaf8e9eb736697d443ae260222c0c1
honuKdestine <honudestine933@gmail.com>
2026-02-18 21:15:14 +0000

    Modify ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.namer_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
